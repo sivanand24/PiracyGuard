99424e65c9efcf86b4143fc47c6aee584f20a1f0
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-04-28 00:37:14 +0530

    working on frontend deployment

---
+++ crawler/src/main/java/com/sportsDetect/crawler/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.sportsDetect.crawler.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+    }
+}
--- crawler/src/main/java/com/sportsDetect/crawler/config/WebConfig.java
+++ crawler/src/main/java/com/sportsDetect/crawler/config/WebConfig.java
@@ -10,7 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://piracy-guard-sivanand24s-projects.vercel.app")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE" ,"OPTIONS")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
