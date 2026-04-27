1c8ebdfe1907919cace6eaebb6c8b15ce98c76f1
Sivanand Mishra <sivanandmishra24@gmail.com>
2026-04-28 01:34:35 +0530

    fixing login error

--- crawler/src/main/java/com/sportsDetect/crawler/CrawlerApplication.java
+++ crawler/src/main/java/com/sportsDetect/crawler/CrawlerApplication.java
@@ -1,5 +1,6 @@
 package com.sportsDetect.crawler;
 
+import com.sportsDetect.crawler.config.SecurityConfig;
 import com.sportsDetect.crawler.model.Media;
 import com.sportsDetect.crawler.repository.MediaRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -15,6 +17,7 @@ import java.time.LocalDateTime;
 
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@Import(SecurityConfig.class)
 @EnableScheduling
 @EnableAsync
 @ComponentScan(basePackages = "com.sportsDetect.crawler")
